fdfce9151cd72b47ee36a3494047c053638c5db2
choyrulh <choirul.humam@students.amikom.ac.id>
2026-01-05 14:24:44 +0700

    change

--- src/components/Projects/myProject.jsx
+++ src/components/Projects/myProject.jsx
@@ -13,7 +13,7 @@ import mongodb from "../../assets/logo/mongoDB.svg";
 import expressJs from "../../assets/logo/expressJS.svg";
 import nodeJs from "../../assets/logo/nodejs.svg";
 import { useInView } from "react-intersection-observer";
-import dronePage from "../../assets/drone-web.webp";
+import xeron from "../../assets/xeron.webp";
 import hotelWeb from "../../assets/hotel-web.webp";
 
 // import { useEffect, useRef } from "react";
@@ -98,9 +98,9 @@ export const Portfolio = () => {
       tags: ["MongoDB", "Express", "React", "Node.js"],
     },
     {
-      title: "Drone Single Page",
-      href: "https://drone-web-spa.vercel.app/",
-      image: dronePage,
+      title: "Animation-With-React",
+      href: "https://animation-react-lilac.vercel.app/",
+      image: xeron,
       tech: [reactjs, tailwindcss, framerMotion],
       type: "Single Page Animation",
       tags: ["React", "TailwindCSS", "Framer-Motion"],
