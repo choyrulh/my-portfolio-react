1767cfcbdc7dc2a36b3e0402488bf673ec8e55ca
choyrulh <choirul.humam@students.amikom.ac.id>
2026-01-28 14:49:12 +0700

    Update Experience.jsx

--- src/components/Resume/Experience.jsx
+++ src/components/Resume/Experience.jsx
@@ -88,9 +88,37 @@ function Experience() {
           <div className="space-y-8 md:space-y-12">
             <ResumeCard
               title="Junior Frontend Developer"
-              subTitle="PT Summit Global Teknologi (Maret - Mei 2023)"
+              subTitle="PT Summit Global Teknologi (Maret 2024 - Maret 2025)"
               result="Tangerang"
-              des="Led frontend development initiatives using React and Next.js, collaborating with cross-functional teams to deliver responsive and performant web applications. Implemented modern UI/UX practices and optimized application performance."
+              des="Lead frontend development initiatives using React and Next.js, collaborating with cross-functional teams to deliver responsive and performant web applications. Implemented modern UI/UX practices and optimized application performance."
+            />
+            {/* Add more ResumeCard components as needed */}
+          </div>
+        </div>
+        <motion.div 
+          variants={titleVariants}
+          className="mb-12 md:mb-16"
+        >
+          <motion.p 
+            className="text-sm text-cyan-500 dark:text-cyan-400 tracking-[4px] mb-2"
+            whileHover={{ x: 10 }}
+          >
+            2025
+          </motion.p>
+        </motion.div>
+
+        <div className="relative">
+          {/* Vertical line */}
+          <div className="absolute left-4 top-0 h-full w-[6px] bg-gray-200 dark:bg-gray-700/30 rounded-full" />
+          
+          {/* Experience cards */}
+          <div className="space-y-8 md:space-y-12">
+            <ResumeCard
+              title="Fullstack Web Developer"
+              subTitle="Pintu Digital Teknologi (Mei 2025 - Now)"
+              result="Pati"
+              des="Lead frontend development initiatives using React 
+              , Next.js, Express.js, collaborating with cross-functional teams to deliver responsive and performant web applications. Implemented modern UI/UX practices and optimized application performance."
             />
             {/* Add more ResumeCard components as needed */}
           </div>
